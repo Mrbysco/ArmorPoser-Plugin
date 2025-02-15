4bce14ecd333998f4bb77b885fd8f67bbd81e2a1
Siebrenvde <siebren@siebrenvde.dev>
2025-02-15 22:31:52 +0100

    Remove redundant listener and fix enableConfigGui
    For Armour Stands, only the PlayerInteractAtEntityEvent fires, making the first listener redundant.
    https://jd.papermc.io/paper/1.21.4/org/bukkit/event/player/PlayerInteractAtEntityEvent.html
    
    Additionally, enableConfigGui was only checked in the first listener, making it effectively useless.
    This is fixed here.

--- src/main/java/com/mrbysco/armorposer/handler/EventHandlers.java
+++ src/main/java/com/mrbysco/armorposer/handler/EventHandlers.java
@@ -9,33 +9,15 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.EquipmentSlot;
 
 public class EventHandlers implements Listener {
-	@EventHandler
-	public void onInteract(PlayerInteractEntityEvent event) {
-		Player player = event.getPlayer();
-		Entity entity = event.getRightClicked();
-		if (entity instanceof ArmorStand armorStand) {
-			if (ArmorPoserPlugin.enableConfigGui && player.isSneaking()) {
-				if (event.getHand() == EquipmentSlot.HAND) {
-					ByteArrayDataOutput out = ByteStreams.newDataOutput();
-					out.writeInt(armorStand.getEntityId());
-					ArmorPoserPlugin.Plugin.getServer().getMessenger().registerOutgoingPluginChannel(ArmorPoserPlugin.Plugin, "armorposer:screen_packet");
-					player.sendPluginMessage(ArmorPoserPlugin.Plugin, "armorposer:screen_packet", out.toByteArray());
-					ArmorPoserPlugin.Plugin.getServer().getMessenger().unregisterOutgoingPluginChannel(ArmorPoserPlugin.Plugin, "armorposer:screen_packet");
-				}
-				event.setCancelled(true);
-			}
-		}
-	}
 
 	@EventHandler
 	public void onInteract(PlayerInteractAtEntityEvent event) {
 		Player player = event.getPlayer();
 		Entity entity = event.getRightClicked();
-		if (entity instanceof ArmorStand armorStand && player.isSneaking()) {
+		if (entity instanceof ArmorStand armorStand && player.isSneaking() && ArmorPoserPlugin.enableConfigGui) {
 			if (event.getHand() == EquipmentSlot.HAND) {
 				ByteArrayDataOutput out = ByteStreams.newDataOutput();
 				out.writeInt(armorStand.getEntityId());
